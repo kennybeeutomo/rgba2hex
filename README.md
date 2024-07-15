6d1821055292509267919bf26fac8c9e7ccc4afa
kennybeeutomo <kennybee132@gmail.com>
2024-07-15 20:11:32 +0700

    Added usage section in readme

--- README.md
+++ README.md
@@ -19,3 +19,24 @@ outputs:
     my-color: #6478FFFF;
 }
 ```
+
+### Usage:
+Usage:
+```
+cat <file> | rgba2hex
+```
+or
+```
+cat <file> | rgba2hex <format>
+```
+```
+<format> = xxxx
+```
+> where x can be \'d\' or \'p\'\
+> d = decimal (0-255)\
+> p = percentage (0-100%)
+
+Example:
+```
+cat file | rgba2hex dddp
+```
--- main.cpp
+++ main.cpp
@@ -84,14 +84,14 @@ void channelsFormat(std::vector<channel>& channels, const std::string& format) {
 }
 
 void printHelp() {
-	std::cout <<	"Usage:\n"
-					"cat <file> | " << PROGRAM_NAME << "\n"
-					"cat <file> | " << PROGRAM_NAME << " <format>\n"
-					"format: xxxx where x can be \'d\' or \'p\'\n"
-					"d = decimal (0-255)\n"
-					"p = percentage (0-100%)\n"
-					"Example:\n"
-					"cat file | " << PROGRAM_NAME << " dddp\n";
+	std::cout << "Usage:\n"
+                 "cat <file> | " << PROGRAM_NAME << "\n"
+                 "cat <file> | " << PROGRAM_NAME << " <format>\n"
+                 "format: xxxx where x can be \'d\' or \'p\'\n"
+                 "d = decimal (0-255)\n"
+                 "p = percentage (0-100%)\n"
+                 "Example:\n"
+                 "cat file | " << PROGRAM_NAME << " dddp\n";
 }
 
 int main(int argc, char** argv) {
